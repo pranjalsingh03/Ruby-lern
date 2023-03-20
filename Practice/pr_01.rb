43c0fc028e4e4424bf34f8f707ed13ca4a9371e7
Pranjal Singh <pranjalsingh9304@gmail.com>
2023-03-20 21:27:17 +0530

    practice of arrays

--- Practice/pr_01.rb
+++ Practice/pr_01.rb
@@ -1,16 +1,21 @@
-$name = 'Pranjal Singh'
-@@var = 18
-class Practice1
-  def name
-    @@var = 18
-    puts "Hello #{$name}"
-  end
+# $name = 'Pranjal Singh'
+# @@var = 18
+# class Practice1
+#   def name
+#     @@var = 18
+#     puts "Hello #{$name}"
+#   end
 
-  def age
-    @@var = 18
-    puts 'Your age is #@var'
-  end
+#   def age
+#     @@var = 18
+#     puts 'Your age is #@var'
+#   end
+# end
+
+# Practice1obj = Practice1.new
+# Practice1obj.name
+
+ary = ['magan', 18.19, 20, 'This is a string']
+ary.each do |i|
+  puts i
 end
-
-Practice1obj = Practice1.new
-Practice1obj.name
